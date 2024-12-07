2b74747ae0b4477482078d085194dc94237bec81
Nipun-Milinda <nipunmilinda@gmail.com>
2024-12-07 10:47:59 +0530

    configure allowRoles middleware and fix lint issues

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,12 +5,21 @@ import {
   deleteCartItem,
   getCartItemsByUser,
 } from '../controllers/cart-controller';
+import allowRoles from '../middlewares/allow-roles';
 
 const router = Router();
 
-router.post('/cart-items', createCartItem);
-router.put('/cart-items/:cart_item_id', updateCartItem);
-router.delete('/cart-items/:cart_item_id', deleteCartItem);
-router.get('/customer/:customer_id/cart-items', getCartItemsByUser);
+router.post('/cart-items', allowRoles('Customer'), createCartItem);
+router.put('/cart-items/:cart_item_id', allowRoles('Customer'), updateCartItem);
+router.delete(
+  '/cart-items/:cart_item_id',
+  allowRoles('Customer'),
+  deleteCartItem,
+);
+router.get(
+  '/customer/:customer_id/cart-items',
+  allowRoles('Customer'),
+  getCartItemsByUser,
+);
 
 export default router;
